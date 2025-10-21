bdfbb164dd56a4dba42cfb67935cab24d3a3b4e4
Ryan <ryan.b.a@live.nl>
2025-10-22 01:39:29 +0200

    Modified ProductRepository.cs **Modified InitializeDatabse method** - Price in create table is now Decimal (was double). **Modified SeedDefaultData**  - defaultItems price now Decimal (was double).  - change prices of items to fit Decimal.  - Removed quotes from item.stock, item.price.  **Modified GetAll**  - Gets data from database now.

--- Grocery.Core.Data/Repositories/ProductRepository.cs
+++ Grocery.Core.Data/Repositories/ProductRepository.cs
@@ -1,11 +1,12 @@
 ﻿using Grocery.Core.Interfaces.Repositories;
 using Grocery.Core.Models;
+using Microsoft.Data.Sqlite;
 
 namespace Grocery.Core.Data.Repositories
 {
     public class ProductRepository : DatabaseConnection, IProductRepository
     {
-        private readonly List<Product> products;
+        private readonly List<Product> products = [];
         public ProductRepository()
         {
             InitializeDatabase();
@@ -26,7 +27,7 @@ namespace Grocery.Core.Data.Repositories
                     Name TEXT NOT NULL,
                     Stock Int Not NULL,
                     ExpirationDate Date NOT NULL,
-                    Price Double Not NULL
+                    Price Decimal Not NULL
                 );
             ";
 
@@ -35,23 +36,44 @@ namespace Grocery.Core.Data.Repositories
 
         private void SeedDefaultData()
         {
-            var defaultItems = new List<(string name, int stock, string expirationDate, double price)>
+            var defaultItems = new List<(string name, int stock, string expirationDate, Decimal price)>
             {
-                ("Melk", 300, "2025-9-25", 0.95),
-                ("Kaas", 100, "2025-9-30", 7.98),
-                ("Brood", 400, "2025-9-12", 2.19),
-                ("Cornflakes", 0, "2025-12-31", 1.48)
+                ("Melk", 300, "2025-09-25", 0.95m),
+                ("Kaas", 100, "2025-09-30", 7.98m),
+                ("Brood", 400, "2025-09-12", 2.19m),
+                ("Cornflakes", 0, "2025-12-31", 1.48m)
             };
 
             var insertQueries = defaultItems.Select(item =>
                 $@"INSERT INTO product(Name, Stock, ExpirationDate, Price)
-                VALUES ('{item.name}', '{item.stock}', '{item.expirationDate}', '{item.price}');"
+                VALUES ('{item.name}', {item.stock}, '{item.expirationDate}', {item.price});"
             ).ToList();
             
             InsertMultipleWithTransaction(insertQueries);
         }
         public List<Product> GetAll()
         {
+            products.Clear();
+            
+            string query = "SELECT * FROM product";
+            
+            OpenConnection();
+
+            using (SqliteCommand command = new SqliteCommand(query, Connection))
+            {
+                SqliteDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    int id = reader.GetInt32(0);
+                    string name = reader.GetString(1);
+                    int stock = reader.GetInt32(2);
+                    DateOnly expirationDate = DateOnly.FromDateTime(reader.GetDateTime(3));
+                    Decimal price = reader.GetDecimal(4);
+                    products.Add(new Product(id, name, stock, expirationDate, price));
+                }
+            }
+            CloseConnection();
             return products;
         }
 
