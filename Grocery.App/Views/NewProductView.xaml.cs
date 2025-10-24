de1a22815e05d81c272e85f5d4494d2927a421c7
Ryan <ryan.b.a@live.nl>
2025-10-24 15:40:15 +0200

    Modified NewProductView.xaml Modified NewProductView.xaml.cs

--- Grocery.App/Views/NewProductView.xaml.cs
+++ Grocery.App/Views/NewProductView.xaml.cs
@@ -1,15 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Grocery.App.ViewModels;
 
 namespace Grocery.App.Views;
 
 public partial class NewProductView : ContentPage
 {
-    public NewProductView()
+    public NewProductView(NewProductViewModel viewModel)
     {
         InitializeComponent();
+        BindingContext = viewModel;
     }
 }
